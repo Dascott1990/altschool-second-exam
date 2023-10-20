32fcdfaee14acda0c1375d188d79a40930ec823d
Mac <mac@Macs-MacBook-Pro.local>
2023-10-20 21:49:22 +0100

    First commit

---
+++ src/routes/ErrorBoundaryTest.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../style.css";
+import { useState } from "react";
+import ErrorBoundary from "./ErrorBoundary";
+function ErrorBoundaryTest() {
+  const [hasError, setHasError] = useState(false);
+
+  if (hasError) {
+    throw new Error(
+      "gosh!.....  there seems to be an error somewhere in this page "
+    );
+  }
+  return (
+    <div className="errorpagetest">
+      <h1>Error Boundary Test :</h1>
+      <p>
+        <button className="errorbtn" onClick={() => setHasError(true)}>
+          ErrorBoundary test
+        </button>
+        <ErrorBoundary />
+      </p>
+    </div>
+  );
+}
+
+export default ErrorBoundaryTest;
---
+++ src/routes/homepage.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "../style.css";
+function Homepage() {
+  return (
+    <>
+      <h1 className="homepage">Welcome to Dascott's Front-End Playground.</h1>
+    </>
+  );
+}
+export default Homepage;
